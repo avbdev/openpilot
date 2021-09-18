4c31d5f3d25f8e3a61ec18b8ead93412ad1befa7
Harald Schafer <harald.the.engineer@gmail.com>
2021-09-17 22:27:46 -0700

    misch cleanup

--- selfdrive/controls/tests/test_following_distance.py
+++ selfdrive/controls/tests/test_following_distance.py
@@ -4,8 +4,7 @@ import numpy as np
 
 from cereal import log
 import cereal.messaging as messaging
-from selfdrive.config import Conversions as CV
-from selfdrive.controls.lib.lead_mpc_lib.lead_mpc import RW, LeadMpc
+from selfdrive.controls.lib.longitudinal_mpc_lib.long_mpc import RW, LongitudinalMpc
 
 
 class FakePubMaster():
@@ -13,21 +12,19 @@ class FakePubMaster():
     assert data
 
 
-def run_following_distance_simulation(v_lead, t_end=200.0):
-  dt = 0.2
+def run_following_distance_simulation(v_lead, t_end=100.0):
+  dt = 0.05
   t = 0.
 
-  x_lead = 200.0
+  x_lead = 140.0
 
   x_ego = 0.0
   v_ego = v_lead
   a_ego = 0.0
 
-  mpc = LeadMpc(0)
+  mpc = LongitudinalMpc()
 
-  first = True
   while t < t_end:
-
     # Setup CarState
     CS = messaging.new_message('carState')
     CS.carState.vEgo = float(v_ego)
@@ -45,10 +42,7 @@ def run_following_distance_simulation(v_lead, t_end=200.0):
 
     # Run MPC
     mpc.set_cur_state(v_ego, a_ego)
-    if first:  # Make sure MPC is converged on first timestep
-      for _ in range(20):
-        mpc.update(CS.carState, radarstate.radarState, 0)
-    mpc.update(CS.carState, radarstate.radarState, 0)
+    mpc.update(CS.carState, radarstate.radarState, 50)
 
     # Choose slowest of two solutions
     v_ego, a_ego = float(mpc.v_solution[5]), float(mpc.a_solution[5])
@@ -57,20 +51,20 @@ def run_following_distance_simulation(v_lead, t_end=200.0):
     x_lead += v_lead * dt
     x_ego += v_ego * dt
     t += dt
-    first = False
 
   return x_lead - x_ego
 
 
 class TestFollowingDistance(unittest.TestCase):
   def test_following_distanc(self):
-    for speed_mph in np.linspace(10, 100, num=10):
-      v_lead = float(speed_mph * CV.MPH_TO_MS)
+    for speed in np.arange(0, 50, 5):
+      print(f'Testing {speed} m/s')
+      v_lead = float(speed)
 
       simulation_steady_state = run_following_distance_simulation(v_lead)
       correct_steady_state = RW(v_lead, v_lead) + 4.0
 
-      self.assertAlmostEqual(simulation_steady_state, correct_steady_state, delta=.2)
+      self.assertAlmostEqual(simulation_steady_state, correct_steady_state, delta=(correct_steady_state*.05 + .3))
 
 
 if __name__ == "__main__":
